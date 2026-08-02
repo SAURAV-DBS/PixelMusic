486659d63e9fb68b445d8228ef3aaab24f017668
Saurav Biswas <sauravbiswas152007@gmail.com>
2026-08-02 18:02:25 +0530

    Update InAppUpdater.kt

--- app/src/main/java/com/unshoo/pixelmusic/utils/InAppUpdater.kt
+++ app/src/main/java/com/unshoo/pixelmusic/utils/InAppUpdater.kt
@@ -46,7 +46,7 @@ sealed class UpdateState {
 object InAppUpdater {
     private val client = OkHttpClient()
     private val gson = Gson()
-    private const val REPO_URL = "https://api.github.com/repos/Saurav-02/PixelMusic/releases/latest"
+    private const val REPO_URL = "https://pixelmusic-updater.atappu805.workers.dev/latest"
 
     suspend fun checkForUpdate(currentVersion: String): UpdateState = withContext(Dispatchers.IO) {
         try {
